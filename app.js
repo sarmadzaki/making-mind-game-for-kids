7d7e8a209c6dd958bb30e05c27f98916d71f10b5
sam <sarmad.zaki@yahoo.com>
2017-04-07 19:27:30 +0500

    game for kids

---
+++ app.js
@@ -0,0 +1,8 @@
+var num = Math.floor(Math.random() * 20);
+alert("Imagine a Number in your Mind");
+alert("Now take the same amount from your friend that you've imagined and add it to your amount");
+alert("Now add " + num + " to your amount and sum");
+num = num/2;
+alert("Now give the half of your amount to the beggar");
+alert("And return the amount of your friend that you have taken");
+document.write("<h1>you have " + num + " left</h1>");
